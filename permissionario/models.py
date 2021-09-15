bd9f28e026d15bb34c79ccc22dd06c0b32d2da61
Amarildo <amarildop1dev@gmail.com>
2021-09-15 19:48:12 -0300

    Página inicial funcionando

--- permissionario/models.py
+++ permissionario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Permissionario(models.Model):
+    nome = models.CharField(max_length=40, verbose_name='Nome')
+    cpf = models.CharField(max_length=40, verbose_name='CPF')
+    telefone = models.CharField(max_length=15, verbose_name='Telefone')
+
+    def _str__(self):
+        return f'Nome: {nome} | Telefone: {telefone}'
+
+
---
+++ permissionario/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', PaginaInicial.as_view(), name='pagina-inicial'),
+]
